f3bd8dcca62d8bf89ea9d5c9a8a46e17477b6966
JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET <75049883+JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET@users.noreply.github.com>
2021-04-20 21:37:32 +1000

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ div.className = "g-b g-b--2of12";
 div.style = "width: 20%; max-width: 20%;";
 div.innerHTML = `<a href="/race/" class="btn btn--primary animate--iconSlam"><svg class="icon icon-friends btn-icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/dist/site/images/icons/icons.css.svg#icon-friends"></use></svg>Start a Friends Race</a>`;
 header.append(div);
-
+document.getElementsByClassName("g-b g-b--10of12")[0].style = "width: 63.33333%; max-width: 63.33333%;";
 document.getElementsByClassName("btn btn--primary animate--iconSlam")[1].href = "https://www.nitrotype.com/race/" + nitro;
 }
